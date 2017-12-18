ac2f669d9d23ee147a6bef3a97128222de6d07ea
uzaiqq <uzair.qq@outlook.com>
2017-12-18 12:16:00 +0500

    created first component .. class,metadata and component and imorting that componnent from angularcore

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector :'pm-root',
+template:`
+<div><h1>{{pageTitle}}</h1></div>
+<div>My First Component </div>
+`
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+
+export class AppComponent{
+  pageTitle:string="Uzair Muhammad Iqbal";
 }
