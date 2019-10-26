5ae8aa5f034c31b9fdb7a93722e4be3093fd170e
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-27 03:46:16 +0500

    Nesting Component In Each Other

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
+import { ServersComponent } from "./servers/servers.component";
 
 @NgModule({
   declarations: [AppComponent, ServerComponent, ServersComponent],
