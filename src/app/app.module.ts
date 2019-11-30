94d8e8853ee2d8475c19f955f51b42ab3a469db0
uzairiqbal <uzair.qq@outlook.com>
2019-11-30 19:58:05 +0500

    For Two Way Binding Imported Forms Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from "./servers/servers.component";
-import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
-import { InformationAlertComponent } from "./information-alert/information-alert.component";
-import {ReactiveFormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
+import {ServerComponent} from "./server/server.component";
+import {ServersComponent} from "./servers/servers.component";
+import {WarningAlertComponent} from "./warning-alert/warning-alert.component";
+import {InformationAlertComponent} from "./information-alert/information-alert.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    InformationAlertComponent
-  ],
-    imports: [BrowserModule, ReactiveFormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ServerComponent,
+        ServersComponent,
+        WarningAlertComponent,
+        InformationAlertComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
