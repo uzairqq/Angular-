ff25292ffb159cf09e50ac863993a6915639d074
uzairiqbal <uzair.qq@outlook.com>
2019-11-30 13:55:12 +0500

    Nested Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from "./servers/servers.component";
+import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
+import { InformationAlertComponent } from "./information-alert/information-alert.component";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    InformationAlertComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/information-alert/information-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-information-alert",
+  templateUrl: "./information-alert.component.html",
+  styleUrls: ["./information-alert.component.css"]
+})
+export class InformationAlertComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
