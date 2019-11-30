8d84d234f86389ada07ebd748b260a9462add2d5
uzairiqbal <uzair.qq@outlook.com>
2019-11-30 15:29:07 +0500

    Used Property Binding To Bind Html Property With TS

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,17 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
-  selector: "app-server",
-  templateUrl: "server.component.html",
-  styleUrls: ["server.component.css"]
+    selector: "app-server",
+    templateUrl: "server.component.html",
+    styleUrls: ["server.component.css"]
 })
 export class ServerComponent {
-  serverId: number = 19;
-  serverName: string = "ConnectServer";
+    serverId: number = 19;
+    serverName: string = "ConnectServer";
+    serverStatus: string = "Online";
 
-  getServerStatus(){
-    return "Online"
-  }
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
 }
