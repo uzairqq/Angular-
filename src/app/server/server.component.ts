7d40031c00e1ce7f31077f0490206100040e032a
uzairiqbal <uzair.qq@outlook.com>
2019-11-30 13:53:03 +0500

    String Interpolation Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,4 +5,11 @@ import { Component } from "@angular/core";
   templateUrl: "server.component.html",
   styleUrls: ["server.component.css"]
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 19;
+  serverName: string = "ConnectServer";
+
+  getServerStatus(){
+    return "Online"
+  }
+}
