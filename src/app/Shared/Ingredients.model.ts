ccb09a361ea12cfab4058d798f1452506959a32a
uzairiqbal <uzair.qq@outlook.com>
2019-12-09 00:09:11 +0500

    Creating the Ingredient Model

---
+++ src/app/Shared/Ingredients.model.ts
@@ -0,0 +1,9 @@
+export class IngredientsModel {
+    public name;
+    public amount;
+
+    constructor(name: string, amount: string) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IngredientsModel} from "../Shared/Ingredients.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
 
-  constructor() { }
+    ingredients: IngredientsModel[] = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
