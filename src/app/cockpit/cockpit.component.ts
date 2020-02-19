ffdaf2f2decd290cea3c60d1d95dc917f69a9bfd
Uzair Iqbal <uzair.qq@outlook.com>
2020-02-19 14:43:26 +0500

    Splitting AppComponent Into Multiple Components(cockpit and serverElements Component)

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-cockpit',
+    templateUrl: './cockpit.component.html',
+    styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+    newServerContent: '';
+    newServerName: '';
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+
+    onAddServer() {
+        this.serverElements.push({
+            type: 'server',
+            name: this.newServerName,
+            content: this.newServerContent
+        });
+    }
+
+    onAddBluePrint() {
+      this.serverElements.push({
+        type: 'blueprint',
+        name: this.newServerName,
+        content: this.newServerContent
+      });
+    }
+}
