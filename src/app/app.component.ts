83f5370a4798004d1eff9a8075c3a833a8a4431b
Uzair Iqbal <uzair.qq@outlook.com>
2020-02-19 14:21:44 +0500

    Removed All Last Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,39 +6,5 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'FirstProjectTheBasics';
-
-    messageToPass = 'Data Successfully Passed!';
-
-    messageToSend: string = 'Hello Angular 8';
-
-    exampleParentProperty: string;
-
-    serverElements = [
-        {type: 'server', name: 'TestServer1', content: 'Just a String1'},
-        {type: 'server', name: 'TestServer2', content: 'Just a String2'},
-        {type: 'server', name: 'TestServer3', content: 'Just a String3'}
-
-    ];
-
-    exampleMethodParent($event) {
-        this.exampleParentProperty = $event;
-    }
-
-    onsServerAdded(serverData: { serverName: string, serverContent: string }) {
-        this.serverElements.push({
-            type: 'server',
-            name: serverData.serverName,
-            content: serverData.serverContent
-        })
-    }
-
-    onBluePrintAdded(blueprintData: { blueprintName: string, serverContent: string }) {
-        this.serverElements.push({
-            type: 'blueprint',
-            name: blueprintData.blueprintName,
-            content: blueprintData.serverContent
-        })
-    }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HeaderComponent} from "./header/header.component";
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
-import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailsComponent,
-    RecipesItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    CockpitComponent,
-    ServerElementComponent  
   ],
     imports: [
         BrowserModule,
